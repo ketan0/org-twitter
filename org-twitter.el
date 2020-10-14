196bb8c3ad38b0ab7ea53b9e2629ccc60f0232e6
Ketan Agrawal <agrawalk@stanford.edu>
2020-10-13 21:42:35 -0400

    first commit

---
+++ org-twitter.el
@@ -0,0 +1,25 @@
+;;; org-twitter.el --- Tweet from org-mode -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Ketan Agrawal
+;;
+;; Author: Ketan Agrawal <http://github/ketan0>
+;; Maintainer: Ketan Agrawal <agrawalk@stanford.edu>
+;; Created: October 13, 2020
+;; Modified: October 13, 2020
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/ketanagrawal/org-twitter
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Tweet from org-mode.
+;;
+;;; Code:
+
+
+
+(provide 'org-twitter)
+;;; org-twitter.el ends here
